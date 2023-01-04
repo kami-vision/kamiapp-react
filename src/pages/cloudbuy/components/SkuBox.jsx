7f769417361ebb7b70e6a1469ec402f7519d6412
vera951004 <yanfeng.geng@kamivision.com>
2023-01-04 14:47:35 +0800

    init

--- src/pages/cloudbuy/components/SkuBox.jsx
+++ src/pages/cloudbuy/components/SkuBox.jsx
@@ -1,37 +1,55 @@
 import React, { useEffect, useState } from "react"
-import { JumboTabs ,Button } from "antd-mobile"
-import './plan.css'
-const Description = ({value}) => {
+import { JumboTabs, Button, Popup } from "antd-mobile"
+import "./plan.css"
+const Description = ({ value }) => {
   return (
     <div className="block">
-     
-      <div className="header">
-        {value.remarks}
-      </div><div className="detail"></div>
-   </div>
-  )
-  
-}
-const BuyButton =({price})=>{
-  console.log('%c [ price ]-13', 'font-size:13px; background:pink; color:#bf2c9f;', price)
-  return (
-     <><div className="btnwrapper"><div>$ {price/100}</div><Button color='primary' fill='solid'>
-     BUY NOW
-   </Button></div></>
+      <div className="header-content">
+        <div className="header">{value.remarks}</div>
+        <div className="line"></div>
+        <div className="price">
+          $<span> {value.price / 100} </span>/{value.serviceCycle == 1 ? "Mo" : "Yr"}
+        </div>
+      </div>
+      <div className="detail"></div>
+    </div>
   )
 }
-const SkuBox = (props) => {
-  const planList = props.planList
-  console.log("%c [ props ]-3", "font-size:13px; background:pink; color:#bf2c9f;", props)
+
+const SkuBox = ({ planList, maxSavePrice, handleSelect }) => {
+  const [activeKey, setActiveKey] = useState("0")
+  const [visible, setVisible] = useState(false)
+
+  const handleChange = (value) => {
+    handleSelect(value)
+    setActiveKey(value)
+  }
+
   return (
     <div className="sku-box">
-      <JumboTabs>
-        {
-        planList?.map((item,index) => {
-          return <JumboTabs.Tab  description={<Description value={item}/>} key={index}> <BuyButton price={item.price}/> </JumboTabs.Tab>
-        })
-        }
+      <JumboTabs activeKey={activeKey} onChange={handleChange}>
+        {planList?.map((item, index) => {
+          return (
+            <JumboTabs.Tab description={<Description value={item} />} key={index}>
+              <div className="btnwrapper">
+                <div>$ {item.price / 100}</div>
+                <Button color="primary" fill="solid" onClick={() => setVisible(true)}>
+                  BUY NOW
+                </Button>
+              </div>
+            </JumboTabs.Tab>
+          )
+        })}
       </JumboTabs>
+      <Popup
+        visible={visible}
+        onMaskClick={() => {
+          setVisible(false)
+        }}
+        bodyStyle={{ height: "40vh", borderTopLeftRadius: "48px", borderTopRightRadius: "48px" }}
+      >
+        <div className="pop-head">You Already Have a Kami Cloud Plan</div>
+      </Popup>
     </div>
   )
 }
--- src/pages/cloudbuy/index.jsx
+++ src/pages/cloudbuy/index.jsx
@@ -1,70 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import { Tabs } from 'antd-mobile'
-import  SkuBox  from './components/SkuBox'
-import { getCloudProductList, getServiceList, setOrderCreateToken, generateOrder, createOrderByServiceCode, getTrial } from '../../api/cloudbuy'
-import { serviceType, channelStatus, SGChannelStatus } from '../../utils/convert'
-import defaultCommonInfo from '../../utils/defaultCommonInfo'
-import bestLoading from '../../utils/bestLoading'
+import React, { useEffect, useState } from "react"
+import { Tabs } from "antd-mobile"
+import SkuBox from "./components/SkuBox"
+import {
+  getCloudProductList,
+  getServiceList,
+  setOrderCreateToken,
+  generateOrder,
+  createOrderByServiceCode,
+  getTrial,
+} from "../../api/cloudbuy"
+import { serviceType, channelStatus, SGChannelStatus } from "../../utils/convert"
+import defaultCommonInfo from "../../utils/defaultCommonInfo"
+import bestLoading from "../../utils/bestLoading"
 
-const Cloudbuy = ({commonProps})=> {
+const Cloudbuy = ({ commonProps }) => {
   const { isAppleTestAccount, deviceType, appName, userInfo } = commonProps
   const [yearlyList, setYearlyList] = useState([])
   const [monthlyList, setMonthlyList] = useState([])
   const [selectedPlan, setSelectedPlan] = useState({})
-  const [maxSavePrice, setMaxSavePrice] = useState('')
-  useEffect(()=>{
+  const [maxSavePrice, setMaxSavePrice] = useState("")
+  useEffect(() => {
     getCloudList()
-  },[])
-  const getCloudList = ()=> {
-    bestLoading('dismissLoading')
+  }, [])
+  const getCloudList = () => {
+    bestLoading("dismissLoading")
     const params = {
       type: 1,
-      channel: userInfo.region == 'SG' ? SGChannelStatus() : channelStatus(deviceType, serviceType(appName))
+      channel: userInfo.region == "SG" ? SGChannelStatus() : channelStatus(deviceType, serviceType(appName)),
     }
     getCloudProductList(params).then((json) => {
       filterPlanList(json?.data?.productsDtos || [])
     })
   }
 
-  const filterPlanList = (data)=> {
-    const yearly = []
-    const monthly = []
-    data.map((item,index)=>{
-      if(item.serviceCycle==1) {
+  const filterPlanList = (data) => {
+    let yearly = []
+    let monthly = []
+    data.forEach((item, index) => {
+      if (item.serviceCycle == 1) {
         monthly.push(item)
-      }else {
+      } else {
         yearly.push(item)
       }
+    })
+    monthly.sort((a, b) => {
+      return b.price - a.price
+    })
+    yearly.sort((a, b) => {
+      return b.price - a.price
     })
     setMonthlyList(monthly)
     setYearlyList(yearly)
   }
-  const getCurrencyCode =  (currency)=> {
+  const getCurrencyCode = (currency) => {
     let defaultInfo = defaultCommonInfo()
-    return defaultInfo.currencySymbol[currency] || '$'
+    return defaultInfo.currencySymbol[currency] || "$"
+  }
+  const handleSelect = (e) => {
+    let save
+    monthlyList[e] &&
+      yearlyList[e] &&
+      (save = ((monthlyList[e]?.price / 100) * 12 - yearlyList[e]?.price / 100).toFixed(2))
+    setMaxSavePrice(save)
   }
   return (
     <>
-    <div>
-      <Tabs color="primary">
-        <Tabs.Tab title='YEARLY' key='yearly'>
-
-        <div className="introduce-cloud">
-          All yearly plans come with an extended <br />
-          18 month hardware warranty for FREE
-        </div>
-          <SkuBox planList={yearlyList}/>
-        </Tabs.Tab>
-        <Tabs.Tab title='MONTHLY' key='monthly'>
-        {maxSavePrice?<div className="save" >
-          Save up to
-          <span>{selectedPlan ? getCurrencyCode(selectedPlan.realCurrency) : '$' } { maxSavePrice} </span>
-          when you pay yearly <br />and get an extended 18 month hardware warranty FREE
-        </div>:null}
-          <SkuBox planList={monthlyList}/>
-        </Tabs.Tab>
-      </Tabs>
-    </div>
+      <div>
+        <Tabs color="primary">
+          <Tabs.Tab title="YEARLY" key="yearly">
+            <div className="introduce-cloud">
+              All yearly plans come with an extended <br />
+              18 month hardware warranty for FREE
+            </div>
+            <SkuBox planList={yearlyList} handleSelect={handleSelect} />
+          </Tabs.Tab>
+          <Tabs.Tab title="MONTHLY" key="monthly">
+            {maxSavePrice ? (
+              <div className="save">
+                Save up to
+                <span>
+                  {selectedPlan ? getCurrencyCode(selectedPlan.realCurrency) : "$"} {maxSavePrice}{" "}
+                </span>
+                when you pay yearly <br />
+                and get an extended 18 month hardware warranty FREE
+              </div>
+            ) : null}
+            <SkuBox planList={monthlyList} maxSavePrice={maxSavePrice} handleSelect={handleSelect} />
+          </Tabs.Tab>
+        </Tabs>
+      </div>
     </>
   )
 }
